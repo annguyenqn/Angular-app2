882604636137450c1dc8f0bfa5ccd8e7a32fb204
annguyenqn <theesan2k1@gmail.com>
2023-07-21 14:37:51 +0700

    subject and operator

--- src/app/RXJS/Observable/Observable.component.ts
+++ src/app/RXJS/Observable/Observable.component.ts
@@ -1,40 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { fromEvent, from, Observable } from 'rxjs';
+import { fromEvent, from, Observable,of } from 'rxjs';
 @Component({
   selector: 'app-Observable',
   templateUrl: './Observable.component.html',
   styleUrls: ['./Observable.component.css']
 })
 export class ObservableComponent implements OnInit {
+   getResult = (num: number) =>{
+    if(num % 2 ==0){
+      throw new Error('invaled number');
 
+    }return num*2
+   }
   constructor() { }
   myObservable = new Observable((observer) => {
     console.log('observable start')
     setTimeout(()=> {
-      observer.next("1")
+      observer.next(this.getResult(1))
     },1000)
     setTimeout(()=> {
-      observer.next("2")// next dùng để phát ra data
+      observer.next(this.getResult(2))// next dùng để phát ra data
     },2000)
     // setTimeout(()=> {
     //   observer.error(new Error('có gì đó sai sai !'))
     // },3000)
     // trong quá trình phát ra data nếu có erro thì các data tiếp theo không được phát ra
     setTimeout(()=> {
-      observer.next("4")
+      observer.next(this.getResult(3))
     },4000)
     setTimeout(()=> {
-      observer.next("5")
+      observer.next(this.getResult(4))
     },5000)
     setTimeout(()=> {
       observer.complete()
     },6000)
     // sau complete thì dù data còn nhưng sẽ không được phát ra
   } )
+  // myObservable2 = Observable.create((observer) => {
 
+  // })
+  // arrayNumber = [1,2,3,4,5]
+  // arrayChar = ['A','B','C','D']
+  // ObserverOfSomething = of(this.arrayNumber,this.arrayChar)
   ngOnInit() {
     //hàm call back đầu tiên thực thi mỗi khi obervable trả về new data, còn call back thứ 2 sẽ được thực thi khi observable trả về erro
-    this.myObservable.subscribe((data) =>{
+    this.myObservable.subscribe( (data) =>{
+
      console.log(data)
     },(error) => {
      // error này sẽ được gán với đới tượng erro ở trong observalbe ở trên
